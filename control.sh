d86b4e0ea8ee6bc4514b1850723daa1e20bd79d6
Ckrenzer <crkrenz72@gmail.com>
2022-06-04 00:57:16 -0400

    Printed message providing instructions on how to turn off Pounce

--- control.sh
+++ control.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+printf "\n\nWarning: Some shell functions have been hidden to pave way for Pounce functionality.\n"
+printf "You can effectively remove Pounce by commenting out the Pounce source call in the .bashrc file."
+
+
 # Wrappers
 # Custom version of the `ls` command
 function ls(){
