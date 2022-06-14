5e0f14124697668242084f4ab3953af2f8a5adda
Ckrenzer <crkrenz72@gmail.com>
2022-06-14 00:33:52 -0400

    fixed issue where relative paths did not identify files

--- src/ls.R
+++ src/ls.R
@@ -10,6 +10,7 @@
 NUM_BYTES_PER_KB <- 2^10
 NUM_BYTES_PER_MB <- 2^20
 NUM_BYTES_PER_GB <- 2^30
+WD <- getwd()
 
 
 # Functions -------------------------------------------------------------------
@@ -41,16 +42,28 @@ determine_unit <- function(size){
 }
 
 # Determines the format to print files.
-determine_message <- function(data){
+determine_message <- function(data, relative_path){
+  if(!length(data) > 0){
+    return(NULL)
+  }
+  
   datefmt <- "   %a, %b %d, %Y at %I:%M %p"
+  # If the files are relative paths (defined as 'paths not starting in a drive'),
+  # ensure the files and directories have the relative paths building up to them
+  # (Ex. 'help/ls.txt' instead of just 'ls.txt').
+  paths <- data
+  if(!any(grepl(x = paths, "^[A-Z]:"))){
+    paths <- paste(relative_path, data, sep = "/")
+  }
+
   # Create a table with the file's:
   # name
   # formatted size
   # most-recent access time
   # most-recent modify time
   # time of creation
-  info <- file.info(data)
-  info$file <- rownames(info)
+  info <- file.info(paths)
+  info$file <- basename(rownames(info))
   info$size <- determine_unit(info$size)
   info$atime <- format(info$atime, format = datefmt)
   info$mtime <- format(info$mtime, format = datefmt)
@@ -68,19 +81,26 @@ determine_message <- function(data){
     }
   }
   
-  print(info)
+  # Only print when data is found
+  if(nrow(info) > 0){
+    print(info)
+  }
   message("")
 }
 
 
 # Interpret Arguments ---------------------------------------------------------
 args <- commandArgs(trailingOnly = TRUE)
-if(length(args) == 0) args <- ""
 
-# Determine the width of the terminal
-# (it should be the first parameter passed).
+# Determine the width of the terminal (it should be the first parameter passed)
+# and then remove it from the args (to avoid interfering with the identification
+# of other parameters).
 width <- as.integer(args[[1]])
 options(width = width)
+args <- args[-1]
+
+# Ensure args has a default value
+if(length(args) == 0) args <- ""
 
 # The help page.
 if(any(args == "--help")){
@@ -96,7 +116,7 @@ if(any(args == "--help")){
 # they are not allowed in file names,
 # at least on Windows).
 subdir <- grep(args, pattern = "/", fixed = TRUE, value = TRUE)
-subdir <- set_default_values(arg = subdir, default_value = getwd())
+subdir <- set_default_values(arg = subdir, default_value = paste0(WD, "/"))
 
 # The regular expression to filter the files in the directory.
 #
@@ -120,8 +140,8 @@ dirs <- grep(x = dirs, pattern = patt, value = TRUE)
 # Print -----------------------------------------------------------------------
 # File Info
 message("FILES:")
-invisible(determine_message(files))
+invisible(determine_message(files, relative_path = subdir))
 
 # Directory Info
 message("DIRS:")
-invisible(determine_message(dirs))
+invisible(determine_message(dirs, relative_path = subdir))
